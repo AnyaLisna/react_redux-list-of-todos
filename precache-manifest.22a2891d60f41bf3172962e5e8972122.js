656d40543453f9d0347bdc83a24d04614f124390
AnyaLisna <anyalesnaya2812@gmail.com>
2019-06-23 22:55:15 +0300

    Updates

---
+++ precache-manifest.22a2891d60f41bf3172962e5e8972122.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158ffccf6d7fbe075861cbb8376b47ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c40642abd65660e3f981",
+    "url": "./static/css/main.a5db45be.chunk.css"
+  },
+  {
+    "revision": "de68364ce413f1000384",
+    "url": "./static/js/2.36016639.chunk.js"
+  },
+  {
+    "revision": "c40642abd65660e3f981",
+    "url": "./static/js/main.ba4cb1c1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
